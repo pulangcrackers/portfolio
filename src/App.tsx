41cea977b1189d307b7bb97fc4e1707e569cecfd
Wayan Danyael Eco <wayaneco29@gmail.com>
2019-10-10 19:15:58 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import NavBar from "./components/Navbar/navBar";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
